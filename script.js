7de4d2ff2d926d9789ade7ca12f67b5ca2b7a865
Shelley McHardy <shelley.mchardy@gmail.com>
2021-05-06 12:37:51 -0400

    prompts with alerts and array built from user preferences

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ function writePassword() {
   passwordText.value = password;
 
 }
+//from Jim Wednesday
+
+function generatePassword() {
+
+
+  return 
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -16,33 +24,75 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var lower = 'abcdefghijklmnopqrstuvwxyz';
-var number = '0123456789'
-var symbol = '!@#$%^&*_+`-=<>?';
-
-//How do we set range of numbers?
-var num = ''
-
-//charAt selects character at a certain position in string
-//Math.floor rounds number down to whole number
-//Math.random produces random number
-
-function generatePassword(num) {
-  var pswd = '';
-  var num = '12lkdsfglkjdfg';
-  for (var i = 0; i < num.length; i++) {
-      pswd += upper.charAt(Math.floor(Math.random() * upper.length));
-      console.log(pswd);
-    }
-  }
-
-generatePassword()
 
 
+// request desired password length
+// parseInt changes a string to a number (w3schools)
+
+function length() {
+  const passwordLength = prompt("Enter a password length between 8 and 128");
+  return parseInt(passwordLength);
+}
+console.log(length())
+
+// request user to select character types and respond with their selection
+// I feel like I should be able to do this with one less variable, but couldn't get it to work
+
+let resultUpper = confirm("Would you like to include upper case letters?");
+let messageUpper = resultUpper
+  if (resultUpper) {
+     messageUpper = 'You clicked OK for upper case';
+  } else {
+     messageUpper = 'You clicked Cancel';
+}
+alert(messageUpper);
+
+let resultLower = confirm("Would you like to include lower case letters?");
+let messageLower = resultLower
+  if (resultLower) {
+     messageLower = 'You clicked OK for lower case';
+  } else {
+     messageLower = 'You clicked Cancel';
+}
+alert(messageLower);
+
+let resultNumber = confirm("Would you like to include numbers?");
+let messageNumber = resultNumber
+  if (resultNumber) {
+     messageNumber = 'You clicked OK for numbers';
+  } else {
+     messageNumber = 'You clicked Cancel';
+}
+alert(messageNumber);
+
+let resultSymbol = confirm("Would you like to include symbols?");
+let messageSymbol = resultSymbol
+  if (resultSymbol) {
+     messageSymbol = 'You clicked OK for symbols';
+  } else {
+     messageSymbol = 'You clicked Cancel';
+}
+alert(messageSymbol);
 
 
+// create array of possible characters from user preferences
 
+let passwordArr = []
 
+if (resultUpper) {
+  passwordArr.push('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+} 
 
+if (resultLower) {
+  passwordArr.push('abcdefghijklmnopqrstuvwxyz');
+}
 
+if (resultNumber) {
+  passwordArr.push('0123456789')
+}
+
+if (resultSymbol) {
+  passwordArr.push('!@#$%^&*_+`-=<>?')
+}
+
+console.log(passwordArr)
